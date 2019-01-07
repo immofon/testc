52007920d5c6651b7a16b55931994bd41d96c2f6
mofon <yzkf000@foxmail.com>
2019-01-07 14:56:01 +0800

    fix

--- main.go
+++ main.go
@@ -14,14 +14,11 @@ func main() {
 		recover()
 		fmt.Println("err")
 	}()
-	fmt.Println("1")
 	c := ebus.NewClient("ws://39.105.42.45:8100/", func(e ebus.Event) {
 	})
-	fmt.Println("2")
 
 	ticker := time.NewTicker(time.Second)
 	for range ticker.C {
-		fmt.Println("evemt")
 		c.Emit(ebus.Event{
 			To:    "@record",
 			Topic: "set",
